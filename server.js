693fc80ff1962137c5ac762a45526b5872ee9c65
Alison Kenney <alisonkenney1@gmail.com>
2016-07-11 10:38:47 -0600

    sprint 1 complete

--- server.js
+++ server.js
@@ -12,37 +12,7 @@ app.use(express.static(__dirname + '/public'));
  * DATABASE *
  ************/
 
-/* hard-coded data */
-var albums = [];
-albums.push({
-              _id: 132,
-              artistName: 'the Old Kanye',
-              name: 'The College Dropout',
-              releaseDate: '2004, February 10',
-              genres: [ 'rap', 'hip hop' ]
-            });
-albums.push({
-              _id: 133,
-              artistName: 'the New Kanye',
-              name: 'The Life of Pablo',
-              releaseDate: '2016, Febraury 14',
-              genres: [ 'hip hop' ]
-            });
-albums.push({
-              _id: 134,
-              artistName: 'the always rude Kanye',
-              name: 'My Beautiful Dark Twisted Fantasy',
-              releaseDate: '2010, November 22',
-              genres: [ 'rap', 'hip hop' ]
-            });
-albums.push({
-              _id: 135,
-              artistName: 'the sweet Kanye',
-              name: '808s & Heartbreak',
-              releaseDate: '2008, November 24',
-              genres: [ 'r&b', 'electropop', 'synthpop' ]
-            });
-
+var db = require('./models');
 
 
 /**********
@@ -74,8 +44,11 @@ app.get('/api', function api_index (req, res){
 });
 
 app.get('/api/albums', function album_index(req, res){
-
-})
+  db.Album.find({}, function(err, albums) {
+    if(err){ return console.log("error :", err);}
+    res.json(albums);
+  });
+});
 
 /**********
  * SERVER *
